e3042c44ad2ad511a2ea2c08a50de05a707fb444
anshu <anshunarayan15nov@gmail.com>
2021-11-03 11:04:12 +0530

    Model files

--- src/model/classification_models.py
+++ src/model/classification_models.py
@@ -1,13 +1,26 @@
 
 from os import stat
+
 from sklearn.linear_model  import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
+
 import pickle
 
 class ClassificationModels:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    @staticmethod
+    def save_model(model):
+        with open(f'E:/iNeuron/Full Stack Data Science/Internship/Projectathon/artifacts/models'+ '/modelForPrediction.sav', 'wb') as f:
+            pickle.dump(model, f)
 
 
     def custom_train_test_split(
@@ -56,12 +69,323 @@ class ClassificationModels:
             n_jobs=n_jobs,
             l1_ratio=l1_ratio)
 
+        return logr
+
+
+    def logistic_regression_classifier(
+        self, 
+        penalty='l2',
+        dual=False,
+        tol=0.0001,
+        C=1.0,
+        fit_intercept=True,
+        intercept_scaling=1,
+        class_weight=None,
+        random_state=None,
+        solver='lbfgs',
+        max_iter=100,
+        multi_class='auto',
+        verbose=0,
+        warm_start=False,
+        n_jobs=None,
+        l1_ratio=None):
+
+        logr = LogisticRegression(
+            penalty=penalty,
+            dual=dual,
+            tol=tol,
+            C=C,
+            fit_intercept=fit_intercept,
+            intercept_scaling=intercept_scaling,
+            class_weight=class_weight,
+            random_state=random_state,
+            solver=solver,
+            max_iter=max_iter,
+            multi_class=multi_class,
+            verbose=verbose,
+            warm_start=warm_start,
+            n_jobs=n_jobs,
+            l1_ratio=l1_ratio)
+
         logr.fit(self.X_train, self.y_train)
 
         ClassificationModels.save_model(logr)
 
     
-    @staticmethod
-    def save_model(model):
-        with open(f'E:/iNeuron/Full Stack Data Science/Internship/Projectathon/artifacts/models'+ '/modelForPrediction.sav', 'wb') as f:
-            pickle.dump(model, f)
+    def decision_tree(
+        self,
+        criterion='gini',
+        splitter='best',
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=None,
+        random_state=None,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        min_impurity_split=None,
+        class_weight=None,
+        ccp_alpha=0.0):
+
+        decisionTree = DecisionTreeClassifier(
+            criterion=criterion,
+            splitter=splitter,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            random_state=random_state,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            min_impurity_split=min_impurity_split,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha)
+
+        return decisionTree
+
+
+    def decision_tree_classifier(
+        self,
+        criterion='gini',
+        splitter='best',
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=None,
+        random_state=None,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        min_impurity_split=None,
+        class_weight=None,
+        ccp_alpha=0.0):
+
+        decisionTree = DecisionTreeClassifier(
+            criterion=criterion,
+            splitter=splitter,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            random_state=random_state,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            min_impurity_split=min_impurity_split,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha)
+
+        decisionTree.fit(self.X_train, self.y_train)
+
+        ClassificationModels.save_model(decisionTree)
+
+
+    def k_neighbors(
+        self,
+        n_neighbors=5,
+        weights='uniform',
+        algorithm='auto',
+        leaf_size=30,
+        p=2,
+        metric='minkowski',
+        metric_params=None,
+        n_jobs=None):
+
+        kNeighborsClassifier = KNeighborsClassifier(
+            n_neighbors=n_neighbors,
+            weights=weights,
+            algorithm=algorithm,
+            leaf_size=leaf_size,
+            p=p,
+            metric=metric,
+            metric_params=metric_params,
+            n_jobs=n_jobs)
+
+        return kNeighborsClassifier
+
+
+    def k_neighbors_classifier(
+        self,
+        n_neighbors=5,
+        weights='uniform',
+        algorithm='auto',
+        leaf_size=30,
+        p=2,
+        metric='minkowski',
+        metric_params=None,
+        n_jobs=None):
+
+        kNeighborsClassifier = KNeighborsClassifier(
+            n_neighbors=n_neighbors,
+            weights=weights,
+            algorithm=algorithm,
+            leaf_size=leaf_size,
+            p=p,
+            metric=metric,
+            metric_params=metric_params,
+            n_jobs=n_jobs
+        )
+
+        kNeighborsClassifier.fit(self.X_train, self.y_train)
+
+        ClassificationModels.save_model(kNeighborsClassifier)
+
+
+    def svc(
+        self,
+        C=1.0,
+        kernel='rbf',
+        degree=3,
+        gamma='scale',
+        coef0=0.0,
+        shrinking=True,
+        probability=False,
+        tol=0.001,
+        cache_size=200,
+        class_weight=None,
+        verbose=False,
+        max_iter=-1,
+        decision_function_shape='ovr',
+        break_ties=False,
+        random_state=None):
+
+        supportVector = SVC(
+            C=C,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state)
+
+        return supportVector
+
+
+    def support_vector_classifier(
+        self,
+        C=1.0,
+        kernel='rbf',
+        degree=3,
+        gamma='scale',
+        coef0=0.0,
+        shrinking=True,
+        probability=False,
+        tol=0.001,
+        cache_size=200,
+        class_weight=None,
+        verbose=False,
+        max_iter=-1,
+        decision_function_shape='ovr',
+        break_ties=False,
+        random_state=None):
+
+        supportVector = SVC(
+            C=C,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state)
+
+        supportVector.fit(self.X_train, self.y_train)
+
+        ClassificationModels.save_model(supportVector)
+
+
+    def bagging_classifier(
+        self,
+        base_estimator=None,
+        n_estimators=10,
+        max_samples=1.0,
+        max_features=1.0,
+        bootstrap=True,
+        bootstrap_features=False,
+        oob_score=False,
+        warm_start=False,
+        n_jobs=None,
+        random_state=None,
+        verbose=0):
+
+        baggingClassifier = BaggingClassifier(
+            base_estimator=base_estimator,
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=max_features,
+            bootstrap=bootstrap,
+            bootstrap_features=bootstrap_features,
+            oob_score=oob_score,
+            warm_start=warm_start,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose)
+
+        baggingClassifier.fit(self.X_train, self.y_train)
+
+        ClassificationModels.save_model(baggingClassifier)
+
+
+    def random_forest_classifier(
+        self,
+        n_estimators=100,
+        criterion='gini',
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features='auto',
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        min_impurity_split=None,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=None,
+        random_state=None,
+        verbose=0,
+        warm_start=False,
+        class_weight=None,
+        ccp_alpha=0.0,
+        max_samples=None):
+
+        randomForestClassifier = RandomForestClassifier(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            min_impurity_split=min_impurity_split,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples)
+
+        randomForestClassifier.fit(self.X_train, self.y_train)
+
+        ClassificationModels.save_model(randomForestClassifier)
