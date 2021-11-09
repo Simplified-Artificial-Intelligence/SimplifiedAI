b965df6b7c2ad990a90fcdbf956a26af32cb137e
Ketan Gangal <Ketangangal98@gmail.com>
2021-11-09 14:57:28 +0530

    Structure fixed + loguru added

--- src/model/custom/classification_models.py
+++ src/model/custom/classification_models.py
@@ -6,10 +6,12 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 
+# Save Model In artifacts
 def save_model(model, path):
     pickle.dump(model, path)
 
 
+# Classification Class for custom training
 class ClassificationModels:
     def __init__(self, X_train, X_test, y_train, y_test, path):
         self.X_train = X_train
@@ -80,6 +82,7 @@ class ClassificationModels:
                                  max_leaf_nodes=None, min_impurity_decrease=0.0, bootstrap=True, oob_score=False,
                                  n_jobs=None, random_state=None, verbose=0, warm_start=False, class_weight=None,
                                  ccp_alpha=0.0, max_samples=None):
+
         model = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion,
                                        max_depth=max_depth, min_samples_split=min_samples_split,
                                        min_samples_leaf=min_samples_leaf,
